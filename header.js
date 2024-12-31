84d91e572b9c52002c9c7be974d36d920f99d3f2
Shigeta117 <masanori.sgt@gmail.com>
2024-12-31 16:38:39 +0900

    ナビゲーション機能拡充

--- header.js
+++ header.js
@@ -1,26 +1,42 @@
 async function loadHeader() {
-    try {
+  try {
       const response = await fetch('header.html');
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
       const headerHtml = await response.text();
       document.body.insertAdjacentHTML('afterbegin', headerHtml);
-    } catch (error) {
-      console.error('ヘッダーの読み込みに失敗しました:', error);
-    }
+      setupHamburgerMenu();
+      setupDropdownMenu(); // ドロップダウンメニューのイベントリスナーを設定
+  } catch (error) {
+    console.error('ヘッダーの読み込みに失敗しました:', error);
   }
-  
-  function setupHamburgerMenu() {
+}
+
+function setupHamburgerMenu() {
     const hamburgerMenu = document.querySelector('.hamburger-menu');
     const nav = document.querySelector('header nav');
-  
+
     if(hamburgerMenu){
         hamburgerMenu.addEventListener('click', () => {
             nav.classList.toggle('active');
         });
     }
-  
+
+}
+function setupDropdownMenu() {
+const dropdownToggle = document.querySelector('.dropdown-toggle');
+  const dropdownMenu = document.querySelector('.dropdown-menu');
+  const dropdown = document.querySelector('.dropdown');
+
+
+if (dropdownToggle && dropdownMenu) {
+    dropdownToggle.addEventListener('click', (event) => {
+    event.preventDefault(); // リンクのデフォルト動作をキャンセル
+          dropdown.classList.toggle('active');
+    });
   }
-  
-  loadHeader().then(() => setupHamburgerMenu());
+}
+
+
+  loadHeader();
